728d50900e4333ee7da63282b861b7ff56f6ec84
Grzegorz <test>
2026-06-16 21:19:42 +0200

    Dopietcie Supabase do rankingu

--- assets/supabase-config.js
+++ assets/supabase-config.js
@@ -1,4 +1,4 @@
 window.WC2026_SUPABASE = Object.freeze({
-  url: "",
-  publishableKey: "",
+  url: "https://npzejjlpyblnmmldcsxr.supabase.co",
+  publishableKey: "sb_publishable_CzDMRvLnfb6SsUmlsPmwlw_cI8vOzTq",
 });
